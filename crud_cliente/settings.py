716d954287d5f28a677ce1d61a2f69f62e5f4b0f
Adson Rodrigues <adson.develop@gmail.com>
2020-12-25 23:17:43 -0300

    feat: adição django rest framework e django auth token.

--- crud_cliente/settings.py
+++ crud_cliente/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +74,13 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 WSGI_APPLICATION = 'crud_cliente.wsgi.application'
 
